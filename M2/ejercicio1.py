d18688e4619699d9b7e46f0d6f4160f3b7dad278
JaviertINC <github@javiertinc.cl>
2022-09-08 20:06:35 -0300

    Ejercicio 1 Terminado

---
+++ M2/ejercicio1.py
@@ -0,0 +1,30 @@
+# Debes crear un programa que dibuje una matriz, según las siguientes consideraciones: 
+# 1. Solicitud de filas 
+# 2. Solicitud de columnas 
+# 3. Se logre dibujar filas indicadas 
+# 4. Se logre dibujar columnas solicitadas. 
+
+##################################################################################################
+
+filas = int(input("Ingresa la cantidad de Filas: "))
+columnas = int(input("Ingresa la cantidad de Columnas: "))
+
+# Hago un ciclo for para imprimir la cantidad de filas indicadas
+for filas in range(filas):
+    # Aquí hago un for para dibujar la parte superior de las columnas hacia al lado.
+    for columnas_superior in range(columnas):
+        print('+---', end="")
+    # Con este pongo el + final para cerrar el dibujo de la parte superior.
+    print("+")
+
+    # Aquí hago un for para dibujar la parte central de las columnas hacia al lado.
+    for columnas_central in range(columnas):
+        print('|   ', end="")
+    # Al igual que el + anterior, aquí cierro el dibujo en la parte central de cada cuadrado.
+    print("|")
+
+#Y un último for para cerrar el dibujo en la parte inferior.
+for columnas_inferior in range(columnas):
+    print('+---', end="")
+# Y aquí el último + para cerrar el dibujo.
+print("+")
